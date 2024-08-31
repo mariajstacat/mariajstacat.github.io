1041d413fc441b0bc5269c0b866b870d87f17f81
MariaIsNotHere <159210278+mariajstacat@users.noreply.github.com>
2024-08-30 18:42:08 -0700

    30/08/2024 practica #1

--- BasedeDatosWeb/parcial1/holamundo.php
+++ BasedeDatosWeb/parcial1/holamundo.php
@@ -1,4 +1,17 @@
 <?php
 
+    $nombre = "Maria";
+    $numero = 8;
+    $numero1 = "8";
 
+    $total = $numero + $numero1;
+    echo "TOTAL = " . $total;
+    
+    echo "<h3> HOLA MUNDO </h3>";
+    for ($i=0; $i<10; $i++){
+        echo $i . "<br>";
+    }
+    echo "<h2 style='color:red;'>Ana María Jacobo Bojórquez </h2>";
+    echo "<img src='https://cdn.segaamerica.com/img/fearless/characters/fearless-tab-shadow.png' style='height: 100px' 'width:230px'>";
+    
 ?>
