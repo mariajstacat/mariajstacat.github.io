f02933d0af9b973560224b00a46617bf770383ad
MariaIsNotHere <159210278+mariajstacat@users.noreply.github.com>
2024-10-04 00:32:26 -0700

    04/10/2023

--- BasedeDatosWeb/parcial2/practica7.js
+++ BasedeDatosWeb/parcial2/practica7.js
@@ -1,11 +1,11 @@
+//this doesnt work and its the same code as the professor u have to be kidding me
 function validar(posicion){
-    var cuadro = document.getElementById("cuadro" + posicion);
-    if (cuadro.value!= "x" && cuadro.value!= "o"){
-        console.log(cuadro.value + "-- valor negado");
+    var cuadro = document.getElementById("cuadro"+posicion);
+    if(cuadro.value != "x" && cuadro.value != "o"){
+        console.log(cuadro.value + "--- nop");
         cuadro.value = '';
-    return;
+        return;
     } else {
-        console.log("prueba");
         var cuadro1 = document.getElementById("cuadro1").value;
         var cuadro2 = document.getElementById("cuadro2").value;
         var cuadro3 = document.getElementById("cuadro3").value;
@@ -18,14 +18,114 @@ function validar(posicion){
 
         switch(posicion){
             case 1:
-                if (cuadro1 == cuadro2 && cuadro1 == cuadro3){
-                    alert("GANADOR :::: " + cuadro1);
-                } else if (cuadro1 == cuadro6 && cuadro1 == cuadro9){
-                    alert("GANADOR :::: " + cuadro1);
-                } else if (cuadro1 == cuadro4 && cuadro1 ==  cuadro7){
-                    alert("GANADOR :::: " + cuadro1);
+                if(cuadro1 == cuadro2 && cuadro1 == cuadro3){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro1)
+                    pintar(1,2,3);
+                } else if(cuadro1 == cuadro5 && cuadro1 == cuadro9){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro1)
+                    pintar(1,5,9);
+                } else if(cuadro1 == cuadro4 && cuadro1 == cuadro7){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro1)
+                    pintar(1,4,7);
                 }
-            break;
+                break;
+            case 2:
+                if(cuadro2 == cuadro1 && cuadro2 == cuadro3){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro2)
+                    pintar(1,2,3);
+                } else if(cuadro2 == cuadro5 && cuadro2 == cuadro8){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro2)
+                    pintar(2,5,8);
+                }
+                break;
+            case 3:
+                if(cuadro3 == cuadro2 && cuadro3 == cuadro1){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro3)
+                    pintar(3,2,1);
+                } else if(cuadro3 == cuadro5 && cuadro3 == cuadro7) {
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro3)
+                    pintar(3,5,7);
+                } else if(cuadro3 == cuadro6 && cuadro3 == cuadro9){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro3)
+                    pintar(3,6,9);
+                }
+                break;
+            case 4:
+                if(cuadro4 == cuadro1 && cuadro4 == cuadro7){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro4)
+                    pintar(4,1,7);
+                } else if(cuadro4 == cuadro5 && cuadro4 == cuadro6){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro4)
+                    pintar(4,5,6);
+                }
+                break;
+            case 5:
+                if(cuadro5 == cuadro2 && cuadro5 == cuadro8){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro5)
+                    pintar(5,2,8);
+                } else if(cuadro5 == cuadro4 && cuadro5 == cuadro6){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro5)
+                    pintar(5,4,6);
+                } else if(cuadro5 == cuadro1 && cuadro5 == cuadro9){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro5)
+                    pintar(5,1,9);
+                } else if(cuadro5 == cuadro3 && cuadro5 == cuadro7){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro5)
+                    pintar(3,5,7)
+                }
+                break;
+            case 6:
+                if(cuadro6 == cuadro5 && cuadro6 == cuadro4){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro6)
+                    pintar(4,5,6);
+                } else if(cuadro6 == cuadro3 && cuadro6 == cuadro9){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro6)
+                    pintar(3,6,9);
+                }
+                break;
+            case 7:
+                if(cuadro7 == cuadro4 && cuadro7 == cuadro1){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro7)
+                    pintar(1,4,7);
+                } else if(cuadro7 == cuadro5 && cuadro7 == cuadro3){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro7)
+                    pintar(7,5,3);
+                } else if(cuadro7 == cuadro8 && cuadro7 == cuadro9){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro7)
+                    pintar(7,8,9);
+                }
+                break;
+            case 8:
+                if(cuadro8 == cuadro5 && cuadro8 == cuadro2){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro8)
+                    pintar(8,5,2);
+                } else if(cuadro8 == cuadro7 && cuadro8 == cuadro9){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro8)
+                    pintar(7,8,9);
+                }
+                break;
+            case 9:
+                if(cuadro9 == cuadro5 && cuadro9 == cuadro1){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro9)
+                    pintar(9,5,1);
+                } else if(cuadro9 == cuadro8 && cuadro9 == cuadro7){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro9)
+                    pintar(9,8,7);
+                } else if(cuadro9 == cuadro6 && cuadro9 == cuadro3){
+                    console.log("WE CANT STOP WINNING ::::::" + cuadro9)
+                    pintar(9,6,3);
+                }
+                break;
         }
     }
+}
+
+function pintar(p1, p2, p3){
+    document.getElementById("cuadro"+p1).style.backgroundColor = "#f5bcf2";
+    document.getElementById("cuadro"+p2).style.backgroundColor = "#f5bcf2";
+    document.getElementById("cuadro"+p3).style.backgroundColor = "#f5bcf2";
+}
+
+function reiniciar(){
+    window.location.href = 'practica7.html';
 }
